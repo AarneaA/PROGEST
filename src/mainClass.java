cdac0ab1560c9e188f1253b365e7a489de485dad
Taavi Kala <xenover@gmail.com>
2014-05-27 22:39:46 +0300

    Read syntax from file

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,15 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import ASTclasses.PuuTipp;
 
 public class mainClass {
-	public static void main(String[] args) {
-	String Code =
-"muut nimi = \"Taavi\"; nimi = \"Aarne\";prindi(nimi); muut limiit = 20; kui limiit < 30 siis {    prindi(\"Limiit on väiksem kui 30!\")} muidu {    prindi(\"Limiit ületatud!\")};iga i korral vahemikus 2..20 {    prindi(i)};muut vanus = 22;kuni vanus < 27 tee {    prindi(\"Auto liising kehtib\");    vanus = vanus + 1;};vanus = 15; tee {prindi(\"Oled veel alaealine! \"); vanus = vanus + 1;} kuni vanus <18;";
-	PuuTipp astree = PROGESTi.generateAST(Code); 
-	PROGESTi.runProgram(astree);
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader("sample_syntax.est"));
+		String code = "";
+		String line;
+		while ((line = reader.readLine()) != null) {
+			code += line;
+		}
+		System.out.println(code);
+		reader.close();
+		PuuTipp astree = PROGESTi.generateAST(code); 
+		PROGESTi.runProgram(astree);
 	
 	/*for(int i = 0; i<astree.getChildren().size(); i++){
 		System.out.println(astree.getChildren().get(i));
 	}*/
 	
-}
+	}
 }
